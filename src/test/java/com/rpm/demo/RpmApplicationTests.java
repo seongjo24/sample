d68dc0b4114f846f8739a8bb558f1794b0fb9168
seongjo24 <53929443+seongjo24@users.noreply.github.com>
2020-01-10 14:57:07 +0900

    first

---
+++ src/test/java/com/rpm/demo/RpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
